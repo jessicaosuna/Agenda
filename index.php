f01aa3b5944abe39323b9d1160dc47d47da50289
Jessica Osuna Diaz <38566879+jessicaosuna@users.noreply.github.com>
2023-07-04 13:15:58 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Agenda de Contactos Web</title>
+    <link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once"dependecias.php"; ?>
+</head>
+
+<body>
+
+</body>
+</html>
