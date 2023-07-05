9a8820e9b33445bcb4b59700cc09efb34504ca7e
Jessica Osuna Diaz <38566879+jessicaosuna@users.noreply.github.com>
2023-07-05 12:05:49 -0600

    Add files via upload

---
+++ vistas/categorias/tablacategorias.php
@@ -0,0 +1,32 @@
+<div class="card">
+  <div class="card-body">
+    <div class="table-responsive">
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Editar</th>
+                    <th>Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <span class="btn btn-warning btn-sm">
+                            <span class="fa-solid fa-pen-to-square"></span>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="btn btn-danger btn-sm">
+                            <span class="fa-solid fa-trash-can"></span>
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</div>
