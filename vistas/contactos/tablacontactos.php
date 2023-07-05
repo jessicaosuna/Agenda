aa36fb1f892bbbbd5f1497981349f02c097cf7eb
Jessica Osuna Diaz <38566879+jessicaosuna@users.noreply.github.com>
2023-07-05 12:07:13 -0600

    Add files via upload

---
+++ vistas/contactos/tablacontactos.php
@@ -0,0 +1,41 @@
+<div class="card">
+  <div class="card-body">
+    <div class="table responsive">
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th>Apellido Paterno</th>
+                    <th>Apellido Materno</th>
+                    <th>Nombre</th>
+                    <th>Teléfono</th>
+                    <th>Email</th>
+                    <th>Categoría</th>
+                    <th>Editar</th>
+                    <th>Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <span class="btn btn-warning btn-sm">
+                            <span class="fa-solid fa-pen-to-square"></span>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="btn btn-danger btn-sm">
+                            <span class="fa-solid fa-trash-can"></span>
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+  </div>
+</div>
